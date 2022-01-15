5603e36c03690c56accb73642890d61ac6e4f487
Soren6666 <liu2430468912@gmail.com>
2022-01-15 17:47:39 +0800

    The first commit for proj0

---
+++ proj0/NBody.java
@@ -0,0 +1,114 @@
+public class NBody {
+    /**
+     * The class that will actually run your simulation.
+     * No construction
+     * @param args
+     * @return
+     */
+
+    public static void main(String[] args){
+        /** 
+         * The main method that launch the whole project.
+         */
+        if(args.length < 1){
+            System.out.println("Please enter command line arguments.");
+            System.out.println("e.g. java NBody <T> <dt> <filename>");
+        }
+        /**
+         * Read the commands line input and convert them to the param.
+         * @param T, dt, filename 
+         *           T: times of the drawing  process.
+         *           dt: intevel of each calculating.
+         *           filename: the location of the img file.
+         */
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        Planet[] planets = readPlanets(filename);
+        double radius = readRadius(filename);
+        In in = new In(filename);
+        int number = in.readInt();
+
+        /**
+         * Draw amd simulate the Planets 
+         */
+        String starfield = "images/starfield.jpg";
+        StdDraw.enableDoubleBuffering();
+        int time = 0;
+        for(time = 0; time <= T; time++){
+            /**
+             * calculate and upgade the param of all planets.
+             */
+            double xForce[] = new double[number]; 
+            double yForce[] = new double[number]; 
+            for(int i = 0; i < number; i++){
+                xForce[i] = planets[i].calcNetForceExertedByX(planets);
+                yForce[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for(int i = 0; i < number; i++){
+                planets[i].update(dt, xForce[i], yForce[i]);
+            }
+
+        StdDraw.setXscale((-1 * radius / 1e+11), radius / 1e+11); // set the scale of the universe.
+        StdDraw.setYscale((-1 * radius / 1e+11), radius / 1e+11);
+        StdDraw.clear();
+        StdDraw.picture(0, 0, starfield);  // set the background.
+        for(int i = 0; i < planets.length; i++){
+            /**
+             * draw all of the planets .
+             */
+            planets[i].draw();
+        }
+        StdDraw.show();
+        StdDraw.pause(10); //set last time.
+        }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                        planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                        planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
+
+
+    }
+
+    public static double readRadius(String args){
+        /**
+         * A method that return a double corresponding to the radius of the universe in that file.
+         * @param args that the location of the txt file.
+         * @return the planet's radius which recode in the txt file.
+         * 
+         * Attention: We call the readRadius method, so we don't need the non-static nethod.
+         */
+        In in = new In(args);   // call in.java source file to find the param in the locate file.
+        int number = in.readInt();
+        double Radius = in.readDouble();
+        return Radius;
+    }
+
+    public static Planet[] readPlanets(String args){
+        /**
+         * A method that return an array of Planets corresponding to the planets in the files.
+         * @param args that the location of the txt file.
+         * @return the planets whose param recoded in the txt file.
+         * 
+         */
+        In in = new In(args);
+        int number = in.readInt();
+        double Radius = in.readDouble();
+        Planet[] planets = new Planet[number] ;
+            for(int i = 0; i < number; i++){
+                double xxPos = in.readDouble();
+                double yyPos = in.readDouble();
+                double xxVel = in.readDouble();
+                double yyVel = in.readDouble();
+                double mass  = in.readDouble();    
+                String imgFileName = in.readString();
+                planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName); // Given the param for Planet
+            }
+        return planets;
+    }
+    
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,123 @@
+import javax.xml.xpath.XPath;
+
+public class Planet {
+    /** 
+     *  The main source file for project 0.
+     */
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xP,double yP,double xV,double yV,double m,String img){
+        /** 
+         * A constructor which include the parmer for Plant
+         */
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass  = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet b){
+        /** 
+         * A constructor which include the first constructor.
+         */
+        this(b.xxPos, b.yyPos, b.xxVel, b.yyVel, b.mass, b.imgFileName); // "this" represent this object that can be called as a non-static method.
+    }
+
+    public double calcDistance(Planet p ){
+        /**
+         * A method that calculates the distance between two Planets.
+         */
+
+        double dis;
+        dis =Math.sqrt((p.yyPos - this.yyPos) * (p.yyPos - this.yyPos) + (p.xxPos - this.xxPos) * (p.xxPos - this.xxPos));
+        return dis;
+    }
+    
+    public double calcForceExertedBy(Planet p){
+        /** 
+         * A method that  calculates the force between two Planets.
+         */
+
+        double force;
+        
+        force = (6.67e-11 * this.mass * p.mass) / (this.calcDistance(p) * this.calcDistance(p));
+        return force;
+    }
+
+    public double calcForceExertedByX(Planet p){
+        /** 
+         * A method that calculates the Xforce between two Planets.
+         */
+        double forceX;
+        forceX = calcForceExertedBy(p) * ((p.xxPos - this.xxPos) / calcDistance(p));
+        return forceX;
+    }
+
+    public double calcForceExertedByY(Planet p){
+        /** 
+         * A method that calculates the Yforce between two Planets.
+         */
+        double forceY;
+        forceY = calcForceExertedBy(p) * ((p.yyPos - this.yyPos) / calcDistance(p));
+        return forceY;
+    }
+
+    public double calcNetForceExertedByX(Planet[] p){
+        /**
+         * A method that calculates the NetforceX among the array of Planets.
+         */
+        double netforceX = 0; 
+        for(int i = 0; i < p.length; i++){
+            if(p[i] == this)
+            continue;
+            else
+            netforceX += this.calcForceExertedByX(p[i]);
+        }
+        return netforceX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p){
+        /**
+         * A method that calculates the NetforceY among the array of Planets.
+         */
+        double netforceY = 0; 
+        for(int i = 0; i < p.length; i++){
+            if(p[i] == this)
+            continue;  // pass it when the planet calculate with itself. 
+            else
+            netforceY += this.calcForceExertedByY(p[i]);
+           }
+        return netforceY;
+    }
+
+    public void update(double dt, double fX, double fY){
+        /**
+         * A method that determines how much the forces exerted on the planet will cause that 
+         * planet to accelerate, and the resulting change in the planet's velocity and position in 
+         * a small period of time dt.
+         */
+        double aX, aY;
+        aX = fX / this.mass;
+        aY = fY / this.mass;
+        this.xxVel = this.xxVel + aX * dt;
+        this.yyVel = this.yyVel + aY * dt;
+        this.xxPos = this.xxPos + this.xxVel * dt;
+        this.yyPos = this.yyPos + this.yyVel * dt;
+    }
+    /**
+     * uses the StdDraw API mentioned above to draw the Body's image at the BOdy's position.
+     */
+    public void draw(){
+        StdDraw.picture(this.xxPos/1e+11, this.yyPos/1e+11, "images/"+ this.imgFileName);
+    }
+
+    }
+
+    
